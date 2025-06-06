f49fec7d310e2559c962a9373bcbb766abb2a835
Danny Hamshananth <hamshananth@gmail.com>
2025-06-06 17:04:57 +0530

    adding a test API

--- app/api/user/[id]/route.jsx
+++ app/api/user/[id]/route.jsx
@@ -1,72 +1,78 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-export default async function GET(req, { params }) {
-  const { id } = params;
+// export default async function handler(req, { params }) {
+//   const { id } = params;
 
-  try {
-    const post = await prisma.user.findUnique({
-      where: { id: Number(id) },
-    });
+//   try {
+//     const post = await prisma.user.findUnique({
+//       where: { id: Number(id) },
+//     });
 
-    if (!post) {
-      return new Response(JSON.stringify({ message: 'User not found' }), {
-        status: 404,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
+//     if (!post) {
+//       return new Response(JSON.stringify({ message: 'User not found' }), {
+//         status: 404,
+//         headers: { 'Content-Type': 'application/json' },
+//       });
+//     }
 
-    return new Response(JSON.stringify(post), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    return new Response(JSON.stringify({ message: 'Error fetching user' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-}
+//     return new Response(JSON.stringify(post), {
+//       status: 200,
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+//   } catch (error) {
+//     return new Response(JSON.stringify({ message: 'Error fetching user' }), {
+//       status: 500,
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+//   }
+// }
 
-export default async function PUT(req, { params }) {
-    const { id } = params;
-    const formData = await req.formData();
+// export default async function PUT(req, { params }) {
+//     const { id } = params;
+//     const formData = await req.formData();
 
-    const name = formData.get('name');
+//     const name = formData.get('name');
     
-    const email = formData.get('email');
+//     const email = formData.get('email');
 
-    try {
-      const updatedUser = await prisma.user.update({
-        where: { id: Number(id) },
-        data: { name, email },
-      });
+//     try {
+//       const updatedUser = await prisma.user.update({
+//         where: { id: Number(id) },
+//         data: { name, email },
+//       });
   
-      return new Response(JSON.stringify(updatedUser), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    } catch (error) {
-      return new Response(JSON.stringify({ message: 'Error updating user' }), {
-        status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
-}
+//       return new Response(JSON.stringify(updatedUser), {
+//         status: 200,
+//         headers: { 'Content-Type': 'application/json' },
+//       });
+//     } catch (error) {
+//       return new Response(JSON.stringify({ message: 'Error updating user' }), {
+//         status: 500,
+//         headers: { 'Content-Type': 'application/json' },
+//       });
+//     }
+// }
 
-export default async function DELETE(req, { params }) {
-    const { id } = params;
+// export default async function DELETE(req, { params }) {
+//     const { id } = params;
   
-    try {
-      await prisma.user.delete({
-        where: { id: Number(id) },
-      });
+//     try {
+//       await prisma.user.delete({
+//         where: { id: Number(id) },
+//       });
   
-      return new Response(null, { status: 204 });
-    } catch (error) {
-      return new Response(JSON.stringify({ message: 'Error deleting user' }), {
-        status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
+//       return new Response(null, { status: 204 });
+//     } catch (error) {
+//       return new Response(JSON.stringify({ message: 'Error deleting user' }), {
+//         status: 500,
+//         headers: { 'Content-Type': 'application/json' },
+//       });
+//     }
+// }
+
+export default async function handler(req, res) {
+  if (method === 'GET') {
+    return new Response(JSON.stringify({ message: 'User not found' }))
+  }
 }
